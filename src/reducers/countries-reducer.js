75ce9f4250936095a5e518850f5e5ed29c984ab6
ypablo <yfedora@gmail.com>
2019-07-06 00:09:49 +0100

    28.5 react-router

--- src/reducers/countries-reducer.js
+++ src/reducers/countries-reducer.js
@@ -1,15 +1,22 @@
-import { GET_COUNTRIES } from '../actions/actions-countries';
+import { GET_COUNTRIES, GET_COUNTRY } from '../actions/actions-countries';
 import countriesData from '../data/countries.json';
 
-const initialState = [];
+const initialState = {
+    countries: countriesData,
+    selectedCountry: {},
+    visibleCountries: []
+};
 
 const countriesReducer = function (state = initialState, action) {
     switch (action.type) {
         case GET_COUNTRIES:
-            return Object.assign({}, state, {countriesData})
-    }
-
-    return state;
+            return Object.assign({}, state, {countries: state.countries});
+        case GET_COUNTRY:
+            const selectedCountry = state.countries.find(country => country.id == action.id);
+            return Object.assign({}, state, {selectedCountry});
+        default: 
+            return state;   
+    }   
 };
 
 export default countriesReducer;
